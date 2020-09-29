5a66140c0389d40108040f49be6388c2c1bb253d
Luis Sanjuan <luiscode92@gmail.com>
2020-09-28 23:06:14 -0500

    update

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,17 +1,17 @@
 $(document).ready(function () {
-    const amenities = {};
-    $('input:checkbox').click(function () {
-      $(this).each(function () {
-        if (this.checked) {
-          amenities[$(this).data('id')] = $(this).data('name');
-        } else {
-          delete amenities[$(this).data('id')];
-        }
-      });
-      if (Object.values(amenities).length > 0) {
-        $('.amenities h4').text(Object.values(amenities).join(', '));
+  const amenities = {};
+  $('input:checkbox').click(function () {
+    $(this).each(function () {
+      if (this.checked) {
+        amenities[$(this).data('id')] = $(this).data('name');
       } else {
-        $('.amenities h4').html('&nbsp');
+        delete amenities[$(this).data('id')];
       }
     });
+    if (Object.values(amenities).length > 0) {
+      $('.amenities h4').text(Object.values(amenities).join(', '));
+    } else {
+      $('.amenities h4').html('&nbsp');
+    }
   });
+});
